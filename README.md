917636a71e16cc716bcdcd53a1f9b60117102e0a
pedrohm <pedro.macedo@eemovel.com.br>
2024-03-15 19:13:55 -0300

    Adding service

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# serverless-email-attachment-extractor
+# Serverless Email Attachment Processor
+
+The `serverless-email-attachment` project provides a fully serverless solution designed for extracting, formatting, storing, and delivering `.csv` file attachments from emails. Utilizing AWS services, including Lambda and S3, alongside Simple Email Service (SES) triggers, this project automates the handling of email attachments efficiently and scalably.
+
+## Features
+
+- **Email Attachment Extraction**: Automatically extracts `.csv` attachments from incoming emails.
+- **Data Transformation**: Formats the extracted `.csv` files according to predefined rules.
+- **Serverless Deployment**: Utilizes AWS Lambda for processing, ensuring scalability and cost-efficiency.
+- **S3 Integration**: Stores the processed attachments in an S3 bucket, facilitating easy access and management.
+- **Customizable**: Easily adaptable to handle various attachment types and processing logic.
+
+## Folder Structure
+
+```
+serverless-email-attachment/
+│
+├── src/
+│ └── get_email_attachment.py # Main Lambda function for processing emails
+│
+├── serverless.yml # Serverless Framework configuration file
+└── requirements.txt # Python dependencies
+```
+
+## Deployment
+
+### Prerequisites
+
+- AWS account
+- AWS CLI installed and configured
+- Serverless Framework
+
+### Steps
+
+1. **Install Dependencies**
+
+   Ensure you have the Serverless Framework installed. If not, you can install it globally with npm:
+ 
+    ```sh 
+    npm install -g serverless
+    ```
+
+2. **Deploy the Service**
+
+   Navigate to the project directory and deploy the service using the Serverless Framework:
+
+    ```sh
+   serverless deploy
+   ```
+
+   This command deploys your function to AWS Lambda, configures the S3 bucket triggers, and sets up the necessary permissions.
+
+3. **Configure SES**
+
+   Set up an AWS SES receipt rule to forward incoming emails to the S3 bucket configured in the `serverless.yml`. Ensure the emails have `.csv` attachments and make sure you adjust the subject environment variable in the `serverless.yml` to match the emails you want to filter.
+
+## Configuration
+
+- **Environment Variables**: Customize behavior by setting environment variables in `serverless.yml`.
+  - `ENV`: Deployment environment (`dev`, `staging`, or `prod`).
+  - `BUCKET`: The name of the S3 bucket for storing processed attachments.
+  - `TARGET_FOLDER`: The folder within the S3 bucket where attachments are stored.
+  - `SUBJECT`: The email subject line to filter for processing attachments.
+
+- **Serverless Plugins**: Utilizes `serverless-python-requirements` for Python dependency management and `serverless-pseudo-parameters` for easier AWS account parameterization.
+
+## Usage
+
+Once deployed, the service automatically processes incoming emails that are forwarded to the S3 bucket by SES. Emails matching the specified subject line and containing `.csv` attachments are processed. The attachments are formatted and stored in the specified S3 bucket location.
+
+## Contributions
+
+Contributions are welcome! Please fork the repository and submit a pull request with your proposed changes or improvements.
+
+## License
+
+Specify your project's license here, ensuring it's appropriate for your project's usage and distribution.
---
+++ src/get_email_attachment.py
@@ -0,0 +1,96 @@
+import email
+import os
+import logging
+import boto3
+from datetime import datetime
+
+# Setup Logging
+ENV = os.environ.get('ENV')
+LEVEL = 'DEBUG' if ENV in ('DEV', 'STAGING') else 'INFO'
+LOG_LEVEL = logging.DEBUG if LEVEL in ['DEBUG'] else logging.INFO
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(LOG_LEVEL)
+
+BUCKET = os.environ.get('BUCKET')
+TARGET_FOLDER = os.environ.get('TARGET_FOLDER', 'extracted-attachments')
+
+SUBJECT = os.environ.get('SUBJECT')
+
+def handler(event, _context):
+
+    timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+    s3 = boto3.client('s3')
+
+    # Get s3 object contents based on bucket name and object key; in bytes and convert to string
+    data = s3.get_object(Bucket=event['Records'][0]['s3']['bucket']['name'], Key=event['Records'][0]['s3']['object']['key'])
+    contents = data['Body'].read().decode("utf-8")
+
+    # Given the s3 object content is the SES email, get the message content and attachment using email package
+    msg = email.message_from_string(contents)
+    attachment = msg.get_payload()[1]
+    if SUBJECT not in msg['subject'].lower():
+        LOGGER.info(f'Email subject does not match tenant: {msg["subject"]}')
+        return  {
+        'statusCode': 303,
+        'body': f'Email subject does not match tenant: {msg["subject"]}'
+    }
+    store_id = attachment.get_filename().split(".")[0]
+
+    # Write the attachment to a temp location
+    with open('/tmp/attach.csv', 'wb') as attachment_file:
+        attachment_file.write(attachment.get_payload(decode=True))
+
+    # Upload the file at the temp location to destination s3 bucket
+    try:
+        s3.upload_file('/tmp/attach.csv', BUCKET, f'{TARGET_FOLDER}/{timestamp}-{store_id}.csv')
+        LOGGER.debug("File uploaded to S3")
+    except FileNotFoundError:
+        LOGGER.debug("The file was not found")
+
+    output_filename = format_csv(attachment, timestamp)
+
+    update_request_stock(output_filename)
+
+    # Clean up the file from temp location
+    os.remove('/tmp/attach.csv')
+
+    return {
+        'statusCode': 200,
+        'body': 'SES Email received and processed'
+    }
+
+
+def format_csv(attachment, timestamp):
+    output_filename = f'/tmp/{ENV}-{attachment.get_filename().split(".")[0]}-{timestamp}.csv'
+    LOGGER.debug('Writing outfile')
+    with open('/tmp/attach.csv', 'r', encoding='utf-16-le') as infile, open(output_filename, 'w', encoding="utf-8") as outfile:
+        # Skip the second line, remove this if not needed for your use case
+        outfile.write("product_id,stock_on_hand \n")
+
+        lines = infile.readlines()[2:]
+
+        for i, line in enumerate(lines):
+            # Strip removes any spaces and newline characters from the ends
+            stripped_line = line.strip()
+            if stripped_line:  # Check if the line is not empty
+                if stripped_line.find('affected') != -1:
+                    continue
+                # Replace tabs with commas and write to output file
+                if stripped_line.split('\t')[1] == '.0000':
+                    outfile.write(stripped_line.replace(
+                        '\t', ',').replace('.0000', '0'))
+
+                else:
+                    outfile.write(stripped_line.replace(
+                        '\t', ',').replace('.0000', ''))
+
+                if int(i) != int(len(lines) - 3):
+                    # Write a newline to go to the next row
+                    outfile.write('\n')
+    LOGGER.debug('Outfile written')
+    return output_filename
+
+
+def update_request_stock(output_filename):
+    return True
